0e556c64f33bfb7c7dd21a9103486c90fbbd61ac
huib25800 <69386875+huib25800@users.noreply.github.com>
2020-08-08 16:42:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCNKUMvzs52_BfKwOTSQ0PnLU_t_cxy_Zs",
+  authDomain: "huzaib-8dc1a.firebaseapp.com",
+  databaseURL: "https://huzaib-8dc1a.firebaseio.com",
+  projectId: "huzaib-8dc1a",
+  storageBucket: "huzaib-8dc1a.appspot.com",
+  messagingSenderId: "101086425904",
+  appId: "1:101086425904:web:c21c7b98c2f0ef10fbcef5",
+  measurementId: "G-JQDY0KFWPC"
 };
 
      
